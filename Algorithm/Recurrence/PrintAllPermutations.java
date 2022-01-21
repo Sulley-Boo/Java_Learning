533e793bfe7fcee3272850304c0b2227cbf64b7a
Feng Wu <68421259+I-amIronMan@users.noreply.github.com>
2022-01-21 18:24:03 +0800

    Create PrintAllPermutations.java

---
+++ Algorithm/Recurrence/PrintAllPermutations.java
@@ -0,0 +1,41 @@
+package com.javatest;
+
+import java.util.*;
+
+public class Test {
+    /**
+     * 打印一个字符串的全部排列
+     * 打印一个字符串的全部排列，要求不要出现重复的排列
+     *
+     */
+
+    public static List<String> ans;
+    public static void process(char[] chs, int k) {
+        if (k == chs.length) { //base case
+            ans.add(String.valueOf(chs));
+        }
+        boolean[] visit = new boolean[26];
+        for (int i = k;i < chs.length;i++) {
+            // 对第k个字符做决策，第k个字符可以和后面所有的字符交换
+            if (!visit[chs[i] - 'a']) { //对当前的字符做判断，如果访问过，就不在进行下一步操作
+                visit[chs[i] - 'a'] = true;
+                swap(chs, i, k);
+                process(chs, k + 1);
+                swap(chs, i, k);
+            }
+        }
+    }
+    public static void swap(char[] chs, int i, int j) {
+        char tmp = chs[i];
+        chs[i] = chs[j];
+        chs[j] = tmp;
+    }
+    public static void main(String[] args) {
+        String test = "abc";
+        char[] chs = test.toCharArray();
+        ans = new ArrayList<>();
+        process(chs, 0);
+        ans.sort(null);
+        System.out.println(ans);
+    }
+}
