4e698d2056e2c7781815e8586f5e80767840f94d
Feng Wu <68421259+I-amIronMan@users.noreply.github.com>
2022-02-11 16:22:28 +0800

    Create HeapSort.java

---
+++ Algorithm/Sort_Algorithm/HeapSort.java
@@ -0,0 +1,138 @@
+package com.javatest;
+
+
+import java.util.Arrays;
+
+public class Test {
+    /**
+     * 堆排序算法实现。
+     * 堆中索引为index结点的左孩子为2*index+1,右孩子为2*index+2,父节点为(index-1)/2。
+     * 大根堆用于实现升序，小根堆用于实现降序。
+     * 优先级队列结构，就是堆结构。
+     * <p>
+     * 1，先让整个数组都变成大根堆结构，建立堆的过程:
+     * 1)从上到下的方法，时间复杂度为O(N*logN)
+     * 2)从下到上的方法，时间复杂度为O(N)
+     * 2，把堆的最大值和堆末尾的值交换，然后减少堆的大小之后，再去调
+     * 整堆，一直周而复始，时间复杂度为O(N*logN)
+     * 3，堆的大小减小成0之后，排序完成
+     */
+
+
+    public static void heapSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            heapInsert(arr, i);
+        }
+        int size = arr.length;
+        swap(arr, 0, --size);
+        while (size > 0) {
+            heapify(arr, 0, size);
+            swap(arr, 0, --size);
+        }
+    }
+
+    //添加新的元素arr[index],调整添加元素后的堆结构为大根堆
+    public static void heapInsert(int[] arr, int index) {
+        while (arr[index] > arr[(index - 1) / 2]) {
+            swap(arr, index, (index - 1) / 2);
+            index = (index - 1) / 2;
+        }
+    }
+
+    //将当前以index为堆顶的堆结构调整为大根堆
+    public static void heapify(int[] arr, int index, int size) {
+        int left = 2 * index + 1;
+        while (left < size) {
+            int largest = left + 1 < size && arr[left + 1] > arr[left] ? left + 1 : left;
+            largest = arr[index] > arr[largest] ? index : largest;
+            if (largest == index) {
+                break;
+            }
+            swap(arr, index, largest);
+            index = largest;
+            left = 2 * index + 1;
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    // for test
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        System.out.println("测试开始...");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            heapSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                System.out.println("出错了！");
+                break;
+            }
+        }
+    }
+}
