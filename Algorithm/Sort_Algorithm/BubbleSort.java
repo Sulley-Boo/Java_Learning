ddc7d3023feaa468c84743b0bb3f763bf868e487
Feng Wu <68421259+I-amIronMan@users.noreply.github.com>
2022-02-11 23:35:00 +0800

    Create BubbleSort.java

---
+++ Algorithm/Sort_Algorithm/BubbleSort.java
@@ -0,0 +1,43 @@
+package com.javatest;
+
+public class Test {
+    /**
+     * 冒泡排序算法实现。
+     */
+
+    public static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int e = arr.length - 1; e > 0; e--) {
+            for (int i = 0; i < e; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                }
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
+    }
+
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-69, 20, -28, -50, 26, 3, 66, -23};
+        printArray(arr);
+        bubbleSort(arr);
+        printArray(arr);
+    }
+}
