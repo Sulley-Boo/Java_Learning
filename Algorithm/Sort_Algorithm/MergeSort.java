2ccf8578c42eaa205ae937f41194bfc95ded525a
Feng Wu <68421259+I-amIronMan@users.noreply.github.com>
2022-02-12 22:29:11 +0800

    Create MergeSort.java

---
+++ Algorithm/Sort_Algorithm/MergeSort.java
@@ -0,0 +1,64 @@
+package com.javatest;
+
+public class Test {
+    /**
+     * 归并排序算法实现。
+     */
+
+    public static void mergeSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        mergeSort(arr, 0, arr.length - 1);
+    }
+
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l == r) {
+            return;
+        }
+        int mid = l + ((r - l) >> 1);
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+
+    public static void merge(int[] arr, int l, int m, int r) {
+        int[] help = new int[r - l + 1];
+        int p1 = l;
+        int p2 = m + 1;
+        int i = 0;
+        while (p1 <= m && p2 <= r) {
+            if (arr[p1] < arr[p2]) {
+                help[i++] = arr[p1++];
+            } else {
+                help[i++] = arr[p2++];
+            }
+        }
+        while (p1 <= m) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= r) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[l + i] = help[i];
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-69, 20, -28, -50, 26, 3, 66, -23};
+        printArray(arr);
+        mergeSort(arr);
+        printArray(arr);
+    }
+}
