1b51175d010517266f06a35088274517663119f7
Feng Wu <68421259+I-amIronMan@users.noreply.github.com>
2022-02-13 21:45:11 +0800

    Update BinarySearch.java

--- Algorithm/Others/BinarySearch.java
+++ Algorithm/Others/BinarySearch.java
@@ -59,45 +59,45 @@ public class BinarySearch {
         int rightMax = process(arr, mid + 1, R);
         return Math.max(leftMax, rightMax);
     }
-    
+
     //获取局部最小值的索引
     public static int findOneLessValueIndex(int[] arr) {
-		if (arr == null || arr.length == 0) {
-			return -1; // no exist
-		}
-		if (arr.length == 1 || arr[0] < arr[1]) {
-			return 0;
-		}
-		if (arr[arr.length - 1] < arr[arr.length - 2]) {
-			return arr.length - 1;
-		}
-		int left = 1;
-		int right = arr.length - 2;
-		int mid = 0;
-		while (left < right) {
-			mid = (left + right) / 2;
-			if (arr[mid] > arr[mid - 1]) {
-				right = mid - 1;
-			} else if (arr[mid] > arr[mid + 1]) {
-				left = mid + 1;
-			} else {
-				return mid;
-			}
-		}
-		return left;
-	}
+        if (arr == null || arr.length == 0) {
+            return -1; // no exist
+        }
+        if (arr.length == 1 || arr[0] < arr[1]) {
+            return 0;
+        }
+        if (arr[arr.length - 1] < arr[arr.length - 2]) {
+            return arr.length - 1;
+        }
+        int left = 1;
+        int right = arr.length - 2;
+        int mid = 0;
+        while (left < right) {
+            mid = (left + right) / 2;
+            if (arr[mid] > arr[mid - 1]) {
+                right = mid - 1;
+            } else if (arr[mid] > arr[mid + 1]) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return left;
+    }
 
     public static void printArray(int[] arr) {
-		for (int i = 0; i != arr.length; i++) {
-			System.out.print(arr[i] + " ");
-		}
-		System.out.println();
-	}
-    
+        for (int i = 0; i != arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         int[] arr = { 6, 5, 3, 4, 6, 7, 8 };
-		printArray(arr);
-		int index = getLessIndex(arr);
-		System.out.println("index: " + index + ", value: " + arr[index]);
+        printArray(arr);
+        int index = findOneLessValueIndex(arr);
+        System.out.println("index: " + index + ", value: " + arr[index]);
     }
 }
