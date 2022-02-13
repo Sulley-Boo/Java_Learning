8f90515ff61a8ab7b9c8367ecbca8396c1e226aa
Feng Wu <68421259+I-amIronMan@users.noreply.github.com>
2022-02-13 16:01:42 +0800

    Update NQueens.java

--- Algorithm/Recurrence/NQueens.java
+++ Algorithm/Recurrence/NQueens.java
@@ -37,47 +37,47 @@ public class NQueens {
     public static int ways2(int n) {
         //方法2，皇后数量不能超过32
         //方法1的常数级优化
-		if (n < 1 || n > 32) {
-			return 0;
-		}
-		int upperLim = n == 32 ? -1 : (1 << n) - 1;
-		return process2(upperLim, 0, 0, 0);
-	}
+        if (n < 1 || n > 32) {
+            return 0;
+        }
+        int upperLim = n == 32 ? -1 : (1 << n) - 1;
+        return process2(upperLim, 0, 0, 0);
+    }
 
     //colLim 列的限制，1的位置不能放皇后，0的位置可以
     //leftDiaLim 左斜线的限制，1的位置不能放皇后，0的位置可以
     //rightDiaLim 右斜线的限制，1的位置不能放皇后，0的位置可以
-	public static int process2(int upperLim, int colLim, int leftDiaLim, int rightDiaLim) {
-		if (colLim == upperLim) { //base case
-			return 1;
-		}
-		int pos = 0;
-		int mostRightOne = 0;
+    public static int process2(int upperLim, int colLim, int leftDiaLim, int rightDiaLim) {
+        if (colLim == upperLim) { //base case
+            return 1;
+        }
+        int pos = 0;
+        int mostRightOne = 0;
         //所有候选皇后的位置，都在pos上
-		pos = upperLim & (~(colLim | leftDiaLim | rightDiaLim));
-		int res = 0;
-		while (pos != 0) {
-			mostRightOne = pos & (~pos + 1);
-			pos = pos - mostRightOne;
-			res += process2(upperLim, colLim | mostRightOne,
-					(leftDiaLim | mostRightOne) << 1,
-					(rightDiaLim | mostRightOne) >>> 1);
-		}
-		return res;
-	}
+        pos = upperLim & (~(colLim | leftDiaLim | rightDiaLim));
+        int res = 0;
+        while (pos != 0) {
+            mostRightOne = pos & (~pos + 1);
+            pos = pos - mostRightOne;
+            res += process2(upperLim, colLim | mostRightOne,
+                    (leftDiaLim | mostRightOne) << 1,
+                    (rightDiaLim | mostRightOne) >>> 1);
+        }
+        return res;
+    }
     public static void main(String[] args) {
         int n = 14;
         int[] record = new int[n];
-        
+
         long start = System.currentTimeMillis();
         System.out.println(process(record, 0, n));
-		long end = System.currentTimeMillis();
-		System.out.println("cost time: " + (end - start) + "ms");
-        
-        
+        long end = System.currentTimeMillis();
+        System.out.println("cost time: " + (end - start) + "ms");
+
+
         start = System.currentTimeMillis();
-		System.out.println(ways2(n));
-		end = System.currentTimeMillis();
-		System.out.println("cost time: " + (end - start) + "ms");
+        System.out.println(ways2(n));
+        end = System.currentTimeMillis();
+        System.out.println("cost time: " + (end - start) + "ms");
     }
 }
