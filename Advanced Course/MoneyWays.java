d2de9bc5a858352f92c1af29f4a42948d0f156fb
Feng Wu <68421259+I-amIronMan@users.noreply.github.com>
2022-03-26 18:07:52 +0800

    Update MoneyWays.java

--- Advanced Course/MoneyWays.java
+++ Advanced Course/MoneyWays.java
@@ -2,7 +2,10 @@ package com.javatest;
 
 public class MoneyWays {
 
-
+    /**
+    * 现有n1+n2种面值的硬币，其中前n1种为普通币，可以取任意枚，后n2种为纪念币，
+    * 每种最多只能取一枚，每种硬币有一个面值，问能用多少种方法拼出m的面值？
+    */
     public static int moneyWays(int[] common, int[] souvenir, int m) {
         int[][] dp1 = new int[common.length + 1][m + 1];
         int[][] dp2 = new int[souvenir.length + 1][m + 1];
