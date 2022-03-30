1fd5129eab6f9b6fc007f9447f232222f367e458
Feng Wu <68421259+I-amIronMan@users.noreply.github.com>
2022-03-30 21:18:45 +0800

    Update BinarySearch.java

--- Algorithm/Others/BinarySearch.java
+++ Algorithm/Others/BinarySearch.java
@@ -29,21 +29,64 @@ public class BinarySearch {
         return sortedArr[L] == num;
     }
 
-    // 在arr上，找满足>=value的最左位置
-    public static int nearestIndex(int[] arr, int value) {
-        int L = 0;
-        int R = arr.length - 1;
+    // 找大于等于value的最左位置
+    public static int nearestIndexLeft(int[] arr, int value) {
+        int l = 0;
+        int r = arr.length - 1;
         int index = -1;
-        while (L < R) {
-            int mid = L + ((R - L) >> 1);
+        while (l <= r) {
+            int mid = l + ((r - l) >> 1);
             if (arr[mid] >= value) {
+                r = mid - 1;
                 index = mid;
-                R = mid - 1;
             } else {
-                L = mid + 1;
+                l = mid + 1;
             }
         }
         return index;
+    }
+
+    // 在数组的指定区间，找大于等于value的最左位置
+    public static int nearestIndexLeft(int[] arr, int value, int l, int r) {
+        while (l <= r) {
+            int mid = l + ((r - l) >> 1);
+            if (arr[mid] >= value) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+
+    // 求小于等于value的最右位置
+    public static int nearestIndexRight(int[] arr, int value) {
+        int l = 0;
+        int r = arr.length - 1;
+        int index = -1;
+        while (l <= r) {
+            int mid = l + ((r - l) >> 1);
+            if (arr[mid] <= value) {
+                l = mid + 1;
+                index = mid;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return index;
+    }
+
+    // 在数组的指定区间，找小于等于value的最右位置
+    public static int nearestIndexRight(int[] arr, int value, int l, int r) {
+        while (l <= r) {
+            int mid = l + ((r - l) >> 1);
+            if (arr[mid] <= value) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return r;
     }
 
     public static int getMax(int[] arr) {
